c1df7ae88c9466f3d4dcf90e3f5b7ccf5217869e
Jarad Touchberry <justatouch09@gmail.com>
2017-04-25 15:12:33 -0400

    Add files via upload

---
+++ src/com/company/Order.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by jaradtouchberry on 4/21/17.
+ */
+public class Order {
+    private String orderId;
+
+    public Order() {
+    }
+
+    public Order(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
+
+
+
